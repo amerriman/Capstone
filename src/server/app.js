935e3502dd08ff6a55937e42dc4ea442509be98d
amerriman <amm434@gmail.com>
2017-06-26 20:21:19 -0600

    update error handling

--- src/server/app.js
+++ src/server/app.js
@@ -69,7 +69,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -79,11 +79,11 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
 });
 
 
